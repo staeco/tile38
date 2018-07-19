6b876d172e67c02480ce9fdd70a7b839ac3a7231
Steve Lacy <me@slacy.me>
2018-07-19 13:19:47 -0700

    remove build_rev from autobuild to pass in as env

--- build-custom.sh
+++ build-custom.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-export BUILD_REV=$(git rev-parse HEAD)
-
 docker build -f Dockerfile.Custom -t gcr.io/stae-product/tile38:aof-${BUILD_REV} .
 
 docker push gcr.io/stae-product/tile38:aof-${BUILD_REV}
