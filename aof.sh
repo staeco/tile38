faab7648e84d785d5916603fff7821e56a208901
Steve Lacy <me@slacy.me>
2018-10-16 19:38:08 -0700

    updates and pin tile38 version

--- aof.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-sleep 10 # wait for server to start
-
-./tile38-cli readonly yes
-
-# shrink the aof every 5 minutes
-# while true; do ./tile38-cli AOFSHRINK; sleep 300; done
--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# nohup ./aof.sh &
-# nohup cat ./commands.log | ./tile38-cli &
-
 ./tile38-server
