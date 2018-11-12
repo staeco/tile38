9702b2aa62bc1ec61bdbabbe55be726eddcc0d72
Steve Lacy <me@slacy.me>
2018-11-12 11:28:17 -0700

    build env does this already

--- build-custom.sh
+++ build-custom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-docker build -f Dockerfile.Custom -t gcr.io/stae-product/tile38:aof-1.14.0-${BUILD_REV} .
+docker build -f Dockerfile.Custom -t gcr.io/stae-product/tile38:aof-${BUILD_REV} .
 
-docker push gcr.io/stae-product/tile38:aof-1.14.0-${BUILD_REV}
+docker push gcr.io/stae-product/tile38:aof-${BUILD_REV}
