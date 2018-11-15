e2dd1f576c18727651ee5a0cad07baebe39e1fe4
Steve Lacy <me@slacy.me>
2018-11-15 13:39:34 -0700

    Add updated ldflags for go 1.11

--- build-custom.sh
+++ build-custom.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
-export TAG=circleci-1.14.1
+export VERSION=1.14.1
+export TAG=circleci-${VERSION}
+export LDFLAGS="-X github.com/tidwall/tile38/core.Version=${VERSION}"
 
-docker build -f Dockerfile.Custom -t staeco/tile38:${TAG} .
+docker build -f Dockerfile.Custom -t staeco/tile38:${TAG} --build-arg="LDFLAGS=${LDFLAGS}" .
 
 docker push staeco/tile38:${TAG}
