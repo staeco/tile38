46b425967688db2ea41500a68d73f27c96ac15a5
Steve Lacy <me@slacy.me>
2019-10-01 09:58:42 -0700

    add custom files

---
+++ build-custom.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+export VERSION=1.17.6
+export TAG=circleci-${VERSION}
+export LDFLAGS="-X github.com/tidwall/tile38/core.Version=${VERSION}"
+
+docker build -f Dockerfile.Custom -t staeco/tile38:${TAG} --build-arg="LDFLAGS=${LDFLAGS}" .
+
+docker push staeco/tile38:${TAG}
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python_server() {
+  python -m SimpleHTTPServer 8081
+}
+
+python_server & ./tile38-server
